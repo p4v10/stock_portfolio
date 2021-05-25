ac5feffdee7b3f8d09f2afd6599a1cb98f3d3760
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-05-24 22:50:20 -0500

    updated

--- stock.py
+++ stock.py
@@ -8,17 +8,41 @@ import pandas_datareader as web
 st.title('Stock Portfolio')
 
 
-tickers = ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA']
-amount = [22,11,12,23,3,3,10]
+tickers = []
+amounts = []
 prices = []
 total = []
 
-#bool_v = True
+bool_v = True
 
-#if bool_v == True:
-    #ticker = st.multiselect(label='What stock do you want to add?', options=['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA'], key='one')
-    #tickers.append(ticker)
+# Sidebar section
+st.sidebar.header("Please add some stocks")
 
+if (bool_v):
+    ticker = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new")
+    amount = st.sidebar.number_input("How many shares?",1,100,2)
+    tickers.append(ticker)
+    amounts.append(amount)
+    ticker2 = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new3")
+    amount2 = st.sidebar.number_input("How many shares?",1,100,4, key="new2")
+    tickers.append(ticker2)
+    amounts.append(amount2)
+    ticker3 = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new2")
+    amount3 = st.sidebar.number_input("How many shares?",1,100,4, key="new4")
+    tickers.append(ticker3)
+    amounts.append(amount3)
+    ticker4 = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new5")
+    amount4 = st.sidebar.number_input("How many shares?",1,100,4, key="new6")
+    tickers.append(ticker4)
+    amounts.append(amount4)
+    ticker5 = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new7")
+    amount5 = st.sidebar.number_input("How many shares?",1,100,4, key="new8")
+    tickers.append(ticker5)
+    amounts.append(amount5)
+    ticker6 = st.sidebar.selectbox("What stock to add?", ['AAPL', 'TSLA', 'FB', 'MSFT', 'AMZN', 'GOOG', 'NVDA', 'ABNB', 'ALL', 'CMG', 'BAC', 'JPM', 'GME', 'HD'],key="new9")
+    amount6 = st.sidebar.number_input("How many shares?",1,100,4, key="new10")
+    tickers.append(ticker6)
+    amounts.append(amount6)
 
 
 for ticker in tickers:
@@ -26,7 +50,7 @@ for ticker in tickers:
     price = df[-1:]['Close'][0]
     prices.append(price)
     index = tickers.index(ticker)
-    total.append(price * amount[index])
+    total.append(price * amounts[index])
 
 fig, ax = plt.subplots(figsize=(14,7))
 
@@ -40,7 +64,7 @@ ax.set_title('Stock Portfolio Visualizer', color="#EF6C35", fontsize=20)
 
 _, texts, _ = ax.pie(total, labels=tickers, autopct='%1.1f%%', pctdistance=0.8)
 
-my_circle = plt.Circle((0,0), 0.60, color='black')
+my_circle = plt.Circle((0,0), 0.60, color='white')
 plt.gca().add_artist(my_circle)
 
 ax.text(-2, 1, 'PORTFOLIO OVERVIEW', fontsize=14, color='black', verticalalignment='center', horizontalalignment='center')
@@ -51,6 +75,3 @@ for ticker in tickers:
     counter += 0.15
 
 st.pyplot(fig)
-
-# Sidebar section
-st.sidebar.header("Please provide your data")
